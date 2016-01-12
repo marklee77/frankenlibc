dbe0caa1e97949ac2a7fa9ab65fca5d2b8fa8498
Mark Stillwell <mark@stillwell.me>
2016-01-12 12:31:17 +0000

    completely remove rump code from franken/init/fdinit.c

--- franken/init/fdinit.c
+++ franken/init/fdinit.c
@@ -23,45 +23,7 @@ int lkl_netdev_add(union lkl_netdev nd, void *mac);
 int lkl_if_up(int ifindex);
 int lkl_netdev_get_ifindex(int id);
 
-enum rump_etfs_type {
-	RUMP_ETFS_REG,
-	RUMP_ETFS_BLK,
-	RUMP_ETFS_CHR,
-	RUMP_ETFS_DIR,
-	RUMP_ETFS_DIR_SUBDIRS
-};
-
-int rump_pub_etfs_register(const char *, const char *, enum rump_etfs_type);
-
 struct __fdtable __franken_fd[MAXFD];
-
-void
-mkkey(char *k, char *n, const char *pre, int dev, int fd)
-{
-	int i, d;
-	int len = strlen(pre);
-
-	if (fd > 99 || dev > 99) abort();
-	for (i = 0; i < len; i++)
-		*k++ = pre[i];
-	if (dev > 9) {
-		d = (dev / 10) + '0';
-		*k++ = d;
-		dev /= 10;
-	}
-	d = dev + '0';
-	*k++ = d;
-	*k++ = 0;
-
-	if (fd > 9) {
-		d = (fd / 10) + '0';
-		*n++ = d;
-		fd /= 10;
-	}
-	d = fd + '0';
-	*n++ = d;
-	*n++ = 0;
-}
 
 void
 __franken_fdinit()
@@ -109,10 +71,6 @@ __franken_fdinit()
 	}
 }
 
-int rump___sysimpl_open(const char *, int, ...);
-int rump___sysimpl_dup2(int, int);
-int rump___sysimpl_close(int);
-
 static void
 mount_tmpfs(void)
 {
@@ -127,69 +85,13 @@ unmount_atexit(void)
 	ret = lkl_sys_umount("/etc", 0);
 }
 
-static int
-register_reg(int dev, int fd, int flags)
-{
-	char key[16], num[16];
-
-	mkkey(key, num, "/dev/vfile", dev, fd);
-	rump_pub_etfs_register(key, num, RUMP_ETFS_REG);
-	return rump___sysimpl_open(key, flags);
-}
-
 static void
 register_net(int fd)
 {
-	/* FIXME: hehe always fixme tagged.. */
+	/* FIXME: can we dynamically grab the real device address? */
 	lkl_if_up(lkl_netdev_get_ifindex(nd_id));
 	lkl_if_set_ipv4(lkl_netdev_get_ifindex(nd_id), 0x0200010a /* 10.1.0.2 */,
 			24);
-#if 0
-	char key[16], num[16];
-	int ret;
-	int sock;
-	char *addr, *mask, *gw;
-	int af = AF_UNSPEC;
-	struct ifcapreq cap;
-
-	mkkey(key, num, "virt", fd, fd);
-	ret = rump_pub_netconfig_ifcreate(key);
-	if (ret == 0) {
-		sock = rump___sysimpl_socket30(AF_INET6, SOCK_STREAM, 0);
-		if (sock != -1) {
-			rump_pub_netconfig_auto_ipv6(key);
-			rump___sysimpl_close(sock);
-			af = AF_INET6;
-		}
-		sock = rump___sysimpl_socket30(AF_INET, SOCK_STREAM, 0);
-		if (sock != -1) {
-			/* XXX move to autodetect later, but gateway complex */
-			addr = getenv("FIXED_ADDRESS");
-			mask = getenv("FIXED_MASK");
-			gw = getenv("FIXED_GATEWAY");
-			if (addr == NULL || mask == NULL || gw == NULL) {
-				rump_pub_netconfig_dhcp_ipv4_oneshot(key);
-			} else {
-				rump_pub_netconfig_ifup(key);
-				rump_pub_netconfig_ipv4_ifaddr_cidr(key, addr, atoi(mask));
-				rump_pub_netconfig_ipv4_gw(gw);
-			}
-			rump___sysimpl_close(sock);
-			af = AF_INET;
-		}
-		/* enable offload features */
-		sock = rump___sysimpl_socket30(af, SOCK_STREAM, 0);
-		if (sock != -1) {
-			strncpy(&cap.ifcr_name, key, IFNAMSIZ);
-			ret = rump___sysimpl_ioctl(sock, SIOCGIFCAP, &cap);
-			if (ret != -1) {
-				cap.ifcr_capenable = cap.ifcr_capabilities;
-				rump___sysimpl_ioctl(sock, SIOCSIFCAP, &cap);
-			}
-			rump___sysimpl_close(sock);
-		}
-	}
-#endif
 }
 
 static int
@@ -219,28 +121,30 @@ __franken_fdinit_create()
 	int fd;
 	int n_reg = 0, n_block = 0;
 
+    /*
 	if (__franken_fd[0].valid) {
 		fd = register_reg(n_reg++, 0, O_RDONLY);
 		if (fd != -1) {
-			rump___sysimpl_dup2(fd, 0);
-			rump___sysimpl_close(fd);
+			___sysimpl_dup2(fd, 0);
+			___sysimpl_close(fd);
 		}
 	}
 	if (__franken_fd[1].valid) {
 		fd = register_reg(n_reg++, 1, O_WRONLY);
 		if (fd != -1) {
-			rump___sysimpl_dup2(fd, 1);
-			rump___sysimpl_close(fd);
+			___sysimpl_dup2(fd, 1);
+			___sysimpl_close(fd);
 		}
 	}
 
 	if (__franken_fd[2].valid) {
 		fd = register_reg(n_reg++, 2, O_WRONLY);
 		if (fd != -1) {
-			rump___sysimpl_dup2(fd, 2);
-			rump___sysimpl_close(fd);
+			___sysimpl_dup2(fd, 2);
+			___sysimpl_close(fd);
 		}
 	}
+    */
 
 	/* XXX would be nicer to be able to detect a file system,
 	   but this also allows us not to mount a block device.
@@ -269,10 +173,11 @@ __franken_fdinit_create()
 			break;
 		switch (__franken_fd[fd].st.st_mode & S_IFMT) {
 		case S_IFREG:
-			fd = register_reg(n_reg++, fd, __franken_fd[fd].flags & O_ACCMODE);
+            // FIXME: register regular file? when does this happen?
 			break;
 		case S_IFBLK:
-			register_block(n_block++, fd, __franken_fd[fd].flags & O_ACCMODE, __franken_fd[fd].st.st_size, 0);
+			register_block(n_block++, fd, __franken_fd[fd].flags & O_ACCMODE,
+                           __franken_fd[fd].st.st_size, 0);
 			break;
 		case S_IFSOCK:
 			register_net(fd);
