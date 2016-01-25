2a10258294fbf9e206295ccc48432fa4b34bd8d8
Mark Stillwell <mark@stillwell.me>
2016-01-25 00:24:10 +0000

    ha! rump_init code moved into franken/init/start_main.c

--- franken/init/start_main.c
+++ franken/init/start_main.c
@@ -1,6 +1,9 @@
 #include <stdint.h>
-#include <asm/syscalls.h>
+
 #include <errno.h>
+
+#include <lkl_host.h>
+#include <asm/syscalls.h>
 
 #include "init.h"
 
@@ -24,16 +27,17 @@ static pid_t hyp_getpid(void) { return 0; }
 static int hyp_syscall(int num, void *arg, long *retval) {
     int ret = 0;
 
+#if 0
     ret = lkl_syscall(num, (long *)arg);
     if (ret < 0) {
         retval[0] = -ret;
         ret = -1;
     } 
+#endif
 
     return ret;
 }
 static void hyp_execnotify(const char *comm) {}
-
 
 static const struct rumpuser_hyperup hyp = {
     .hyp_schedule = hyp_schedule,
@@ -53,9 +57,103 @@ static const struct rumpuser_hyperup hyp = {
     .hyp_syscall = hyp_syscall,
     .hyp_execnotify = hyp_execnotify,
 };
- 
-int rump_init(void);
 
+static int threads_are_go;
+static struct rumpuser_mtx *thrmtx;
+static struct rumpuser_cv *thrcv;
+
+struct thrdesc {
+	void (*f)(void *);
+	void *arg;
+	int canceled;
+	void *thrid;
+	struct timespec timeout;
+	struct rumpuser_mtx *mtx;
+	struct rumpuser_cv *cv;
+};
+
+static void *rump_timer_trampoline(void *arg)
+{
+	struct thrdesc *td = arg;
+	void (*f)(void *);
+	void *thrarg;
+	int err;
+
+	/* from src-netbsd/sys/rump/librump/rumpkern/thread.c */
+	/* don't allow threads to run before all CPUs have fully attached */
+	if (!threads_are_go) {
+		rumpuser_mutex_enter_nowrap(thrmtx);
+		while (!threads_are_go) {
+			rumpuser_cv_wait_nowrap(thrcv, thrmtx);
+		}
+		rumpuser_mutex_exit(thrmtx);
+	}
+
+	f = td->f;
+	thrarg = td->arg;
+	if (td->timeout.tv_sec != 0 || td->timeout.tv_nsec != 0) {
+		rumpuser_mutex_enter(td->mtx);
+		err = rumpuser_cv_timedwait(td->cv, td->mtx,
+					    td->timeout.tv_sec,
+					    td->timeout.tv_nsec);
+		if (td->canceled) {
+			if (!td->thrid) {
+				rumpuser_free(td, 0);
+			}
+			goto end;
+		}
+		rumpuser_mutex_exit(td->mtx);
+		/* FIXME: we should not use rumpuser__errtrans here */
+		/* FIXME: 60=>ETIMEDOUT(netbsd) rumpuser__errtrans(ETIMEDOUT)) */
+		if (err && err != 60)
+			goto end;
+	}
+
+	rumpuser_free(td, 0);
+	f(thrarg);
+
+	rumpuser_thread_exit();
+end:
+	return arg;
+}
+
+#define NSEC_PER_SEC 1000000000
+
+static void *rump_add_timer(__u64 ns, void (*func) (void *arg), void *arg)
+{
+	int ret;
+	struct thrdesc *td;
+
+	rumpuser_malloc(sizeof(*td), 0, (void **)&td);
+
+	memset(td, 0, sizeof(*td));
+	td->f = func;
+	td->arg = arg;
+	td->timeout = (struct timespec){ .tv_sec = ns / NSEC_PER_SEC,
+					 .tv_nsec = ns % NSEC_PER_SEC};
+
+	rumpuser_mutex_init(&td->mtx, RUMPUSER_MTX_SPIN);
+	rumpuser_cv_init(&td->cv);
+
+	ret = rumpuser_thread_create(rump_timer_trampoline, td, "timer",
+				     1, 0, -1, &td->thrid);
+	if (ret) {
+		rumpuser_free(td, 0);
+		return NULL;
+	}
+
+	return td;
+}
+
+static void *timer_alloc(void (*fn)(void *), void *arg) {
+    return fn;
+}
+
+static int timer_set_oneshot(void *timer_fn, unsigned long ns)
+{
+    return rump_add_timer(ns, (void (*)(void *))timer_fn, NULL) ? 0 : -1;
+} 
+ 
 char **environ __attribute__((weak));
 
 static char empty_string[] = "";
@@ -93,6 +191,9 @@ finifn()
 	_fini();
 }
 
+#define LKL_MEM_SIZE 100 * 1024 * 1024
+static char *boot_cmdline = "";    /* FIXME: maybe we have rump_set_boot_cmdline? */
+
 int
 __franken_start_main(int(*main)(int,char **,char **), int argc, char **argv, char **envp)
 {
@@ -115,7 +216,18 @@ __franken_start_main(int(*main)(int,char **,char **), int argc, char **argv, cha
         return EINVAL;
     }
 
-	rump_init();
+	rumpuser_mutex_init(&thrmtx, RUMPUSER_MTX_SPIN);
+	rumpuser_cv_init(&thrcv);
+	threads_are_go = 0;
+
+	lkl_host_ops.timer_alloc = timer_alloc;
+	lkl_host_ops.timer_set_oneshot = timer_set_oneshot;
+	lkl_start_kernel(&lkl_host_ops, LKL_MEM_SIZE, boot_cmdline);
+
+	rumpuser_mutex_enter(thrmtx);
+    threads_are_go = 1;
+	rumpuser_cv_broadcast(thrcv);
+	rumpuser_mutex_exit(thrmtx);
 
 	__init_libc(envp, argv[0]);
 	__libc_start_init();
