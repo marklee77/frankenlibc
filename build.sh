c100b40025b952f2256fc8b6212723c3db5c1ef5
Mark Stillwell <mark@stillwell.me>
2016-02-07 00:49:49 +0000

    auto checkout lkl-linux and lkl-musl

--- build.sh
+++ build.sh
@@ -4,7 +4,8 @@ MAKE=${MAKE-make}
 
 BUILDDIR=${PWD}/build
 STAGEDIR=${BUILDDIR}/stage
-LKLSRC=${PWD}/../lkl-linux
+LKLSRC=${BUILDDIR}/lkl-linux
+MUSLSRC=${BUILDDIR}/lkl-musl
 OUTDIR=${PWD}/dist
 NCPU=1
 
@@ -14,31 +15,31 @@ TARGET=$(LC_ALL=C ${CC-cc} -v 2>&1 | sed -n 's/^Target: //p' )
 
 case ${TARGET} in
 *-linux*)
-	OS=linux
-	;;
+    OS=linux
+    ;;
 *-netbsd*)
-	OS=netbsd
-	;;
+    OS=netbsd
+    ;;
 *-freebsd*)
-	OS=freebsd
-	FILTER="-DCAPSICUM"
-	;;
+    OS=freebsd
+    FILTER="-DCAPSICUM"
+    ;;
 *)
-	OS=unknown
+    OS=unknown
 esac
 
 HOST=$(uname -s)
 
 case ${HOST} in
 Linux)
-	NCPU=$(nproc 2>/dev/null || echo 1)
-	;;
+    NCPU=$(nproc 2>/dev/null || echo 1)
+    ;;
 NetBSD)
-	NCPU=$(sysctl -n hw.ncpu)
-	;;
+    NCPU=$(sysctl -n hw.ncpu)
+    ;;
 FreeBSD)
-	NCPU=$(sysctl -n hw.ncpu)
-	;;
+    NCPU=$(sysctl -n hw.ncpu)
+    ;;
 esac
 
 STDJ="-j ${NCPU}"
@@ -47,49 +48,48 @@ DBG_F='-O2 -g'
 
 helpme()
 {
-	printf "Usage: $0 [-h] [options] [platform]\n"
-	printf "supported options:\n"
-	printf "\t-L: libraries to link eg net_netinet,net_netinet6. default all\n"
-	printf "\t-m: hardcode memory limit. default from env or unlimited\n"
-	printf "\t-M: thread stack size. default: 64k\n"
-	printf "\t-p: huge page size to use eg 2M or 1G\n"
-	printf "\t-r: release build, without debug settings\n"
-	printf "\t-o: location of object files. default PWD/build\n"
-	printf "\t-d: location of installed files. default PWD/dest\n"
-	printf "\t-b: location of binaries. default PWD/dest/bin\n"
-	printf "\tseccomp|noseccomp: select Linux seccomp (default off)\n"
-	printf "\texecveat: use new linux execveat call default off)\n"
-	printf "\tcapsicum|nocapsicum: select FreeBSD capsicum (default on)\n"
-	printf "\tdeterministic: make deterministic\n"
-	printf "\tnotests: do not run tests\n"
-	printf "\tnotools: do not build extra tools\n"
-	printf "\tclean: clean object directory first\n"
-	printf "\n"
-	printf "Supported platforms are currently: linux, netbsd, freebsd, qemu-arm, spike\n"
-	exit 1
+    printf "Usage: $0 [-h] [options] [platform]\n"
+    printf "supported options:\n"
+    printf "\t-L: libraries to link eg net_netinet,net_netinet6. default all\n"
+    printf "\t-m: hardcode memory limit. default from env or unlimited\n"
+    printf "\t-M: thread stack size. default: 64k\n"
+    printf "\t-p: huge page size to use eg 2M or 1G\n"
+    printf "\t-r: release build, without debug settings\n"
+    printf "\t-o: location of object files. default PWD/build\n"
+    printf "\t-d: location of installed files. default PWD/dest\n"
+    printf "\t-b: location of binaries. default PWD/dest/bin\n"
+    printf "\tseccomp|noseccomp: select Linux seccomp (default off)\n"
+    printf "\ttests|notests: select tests (default on)\n"
+    printf "\ttools|notools: build extra tools (default on)\n"
+    printf "\texecveat: use new linux execveat call default off)\n"
+    printf "\tdeterministic: make deterministic\n"
+    printf "\tclean: clean object directory first\n"
+    printf "\n"
+    printf "Supported platforms are currently: linux, netbsd, freebsd, qemu-arm, spike\n"
+    exit 1
 }
 
 bytes()
 {
-	value=$(echo "$1" | sed 's/[^0123456789].*$//g')
-	units=$(echo "$1" | sed 's/^[0123456789]*//g')
+    value=$(echo "$1" | sed 's/[^0123456789].*$//g')
+    units=$(echo "$1" | sed 's/^[0123456789]*//g')
 
-	case "$units" in
-	"kb"|"k"|"KB"|"K")
-		value=$((${value} * 1024))
-		;;
-	"mb"|"m"|"MB"|"M")
-		value=$((${value} * 1048576))
-		;;
-	"gb"|"g"|"GB"|"G")
-		value=$((${value} * 1073741824))
-		;;
-	*)
-		die "Cannot understand value"
-		;;
-	esac
+    case "$units" in
+    "kb"|"k"|"KB"|"K")
+        value=$((${value} * 1024))
+        ;;
+    "mb"|"m"|"MB"|"M")
+        value=$((${value} * 1024 * 1024))
+        ;;
+    "gb"|"g"|"GB"|"G")
+        value=$((${value} * 1024 * 1024 * 1024))
+        ;;
+    *)
+        die "Cannot understand value"
+        ;;
+    esac
 
-	echo ${value}
+    echo ${value}
 }
 
 abspath() {
@@ -99,108 +99,108 @@ abspath() {
 
 appendvar_fs ()
 {
-	vname="${1}"
-	fs="${2}"
-	shift 2
-	if [ -z "$(eval echo \${$vname})" ]; then
-		eval ${vname}="\${*}"
-	else
-		eval ${vname}="\"\${${vname}}"\${fs}"\${*}\""
-	fi
+    vname="${1}"
+    fs="${2}"
+    shift 2
+    if [ -z "$(eval echo \${$vname})" ]; then
+        eval ${vname}="\${*}"
+    else
+        eval ${vname}="\"\${${vname}}"\${fs}"\${*}\""
+    fi
 }
 
 appendvar ()
 {
 
-	vname="$1"
-	shift
-	appendvar_fs "${vname}" ' ' $*
+    vname="$1"
+    shift
+    appendvar_fs "${vname}" ' ' $*
 }
 
 while getopts '?b:d:F:Hhj:k:L:M:m:o:p:qrs:V:' opt; do
-	case "$opt" in
-	"b")
-		mkdir -p ${OPTARG}
-		BINDIR=$(abspath ${OPTARG})
-		;;
-	"d")
-		mkdir -p ${OPTARG}
-		OUTDIR=$(abspath ${OPTARG})
-		;;
-	"F")
-		ARG=${OPTARG#*=}
-		case ${OPTARG} in
-			CFLAGS\=*)
-				appendvar EXTRA_CFLAGS "${ARG}"
-				;;
-			AFLAGS\=*)
-				appendvar EXTRA_AFLAGS "${ARG}"
-				;;
-			LDFLAGS\=*)
-				appendvar EXTRA_LDFLAGS "${ARG}"
-				;;
-			ACFLAGS\=*)
-				appendvar EXTRA_CFLAGS "${ARG}"
-				appendvar EXTRA_AFLAGS "${ARG}"
-				;;
-			ACLFLAGS\=*)
-				appendvar EXTRA_CFLAGS "${ARG}"
-				appendvar EXTRA_AFLAGS "${ARG}"
-				appendvar EXTRA_LDFLAGS "${ARG}"
-				;;
-			CPPFLAGS\=*)
-				appendvar EXTRA_CPPFLAGS "${ARG}"
-				;;
-			DBG\=*)
-				appendvar F_DBG "${ARG}"
-				;;
-			CWARNFLAGS\=*)
-				appendvar EXTRA_CWARNFLAGS "${ARG}"
-				;;
-			*)
-				die Unknown flag: ${OPTARG}
-				;;
-		esac
-		;;
-	"H")
-		appendvar EXTRAFLAGS "-H"
-		;;
-	"h")
-		helpme
-		;;
-	"j")
-		STDJ="-j ${OPTARG}"
-		;;
-	"L")
-		LIBS="${OPTARG}"
-		;;
-	"M")
-		size=$(bytes ${OPTARG})
-		appendvar FRANKEN_FLAGS "-DSTACKSIZE=${size}"
-		;;
-	"o")
-		mkdir -p ${OPTARG}
-		BUILDDIR=$(abspath ${OPTARG})
-		STAGEDIR=${BUILDDIR}/stage
-		;;
-	"p")
-		SIZE=$(bytes ${OPTARG})
-		HUGEPAGESIZE="-DHUGEPAGESIZE=${SIZE}"
-		;;
-	"q")
-		BUILD_QUIET=${BUILD_QUIET:=-}q
-		;;
-	"r")
-		DBG_F="-O2"
-		appendvar EXTRAFLAGS "-r"
-		;;
-	"V")
-		appendvar EXTRAFLAGS "-V ${OPTARG}"
-		;;
-	"?")
-		helpme
-		;;
-	esac
+    case "$opt" in
+    "b")
+        mkdir -p ${OPTARG}
+        BINDIR=$(abspath ${OPTARG})
+        ;;
+    "d")
+        mkdir -p ${OPTARG}
+        OUTDIR=$(abspath ${OPTARG})
+        ;;
+    "F")
+        ARG=${OPTARG#*=}
+        case ${OPTARG} in
+            CFLAGS\=*)
+                appendvar EXTRA_CFLAGS "${ARG}"
+                ;;
+            AFLAGS\=*)
+                appendvar EXTRA_AFLAGS "${ARG}"
+                ;;
+            LDFLAGS\=*)
+                appendvar EXTRA_LDFLAGS "${ARG}"
+                ;;
+            ACFLAGS\=*)
+                appendvar EXTRA_CFLAGS "${ARG}"
+                appendvar EXTRA_AFLAGS "${ARG}"
+                ;;
+            ACLFLAGS\=*)
+                appendvar EXTRA_CFLAGS "${ARG}"
+                appendvar EXTRA_AFLAGS "${ARG}"
+                appendvar EXTRA_LDFLAGS "${ARG}"
+                ;;
+            CPPFLAGS\=*)
+                appendvar EXTRA_CPPFLAGS "${ARG}"
+                ;;
+            DBG\=*)
+                appendvar F_DBG "${ARG}"
+                ;;
+            CWARNFLAGS\=*)
+                appendvar EXTRA_CWARNFLAGS "${ARG}"
+                ;;
+            *)
+                die Unknown flag: ${OPTARG}
+                ;;
+        esac
+        ;;
+    "H")
+        appendvar EXTRAFLAGS "-H"
+        ;;
+    "h")
+        helpme
+        ;;
+    "j")
+        STDJ="-j ${OPTARG}"
+        ;;
+    "L")
+        LIBS="${OPTARG}"
+        ;;
+    "M")
+        size=$(bytes ${OPTARG})
+        appendvar FRANKEN_FLAGS "-DLKL_MEM_SIZE=${size}"
+        ;;
+    "o")
+        mkdir -p ${OPTARG}
+        BUILDDIR=$(abspath ${OPTARG})
+        STAGEDIR=${BUILDDIR}/stage
+        ;;
+    "p")
+        SIZE=$(bytes ${OPTARG})
+        HUGEPAGESIZE="-DHUGEPAGESIZE=${SIZE}"
+        ;;
+    "q")
+        BUILD_QUIET=${BUILD_QUIET:=-}q
+        ;;
+    "r")
+        DBG_F="-O2"
+        appendvar EXTRAFLAGS "-r"
+        ;;
+    "V")
+        appendvar EXTRAFLAGS "-V ${OPTARG}"
+        ;;
+    "?")
+        helpme
+        ;;
+    esac
 done
 shift $((${OPTIND} - 1))
 
@@ -208,51 +208,45 @@ if [ -z ${BINDIR+x} ]; then BINDIR=${OUTDIR}/bin; fi
 
 for arg in "$@"; do
         case ${arg} in
-	"clean")
-		${MAKE} clean
-		;;
-	"noseccomp")
-		;;
-	"seccomp")
-		appendvar FILTER "-DSECCOMP"
-		appendvar TOOLS_LDLIBS "-lseccomp"
-		;;
-	"noexecveat")
-		;;
-	"execveat")
-		appendvar FILTER "-DEXECVEAT"
-		;;
-	"nocapsicum")
-		FILTER="-DNOCAPSICUM"
-		;;
-	"capsicum")
-		FILTER="-DCAPSICUM"
-		;;
-	"deterministic"|"det")
-		DETERMINISTIC="deterministic"
-		;;
-	"test"|"tests")
-		RUNTESTS="test"
-		;;
-	"notest"|"notests")
-		RUNTESTS="notest"
-		;;
-	"tools")
-		MAKETOOLS="yes"
-		;;
-	"notools")
-		MAKETOOLS="no"
-		;;
-	*)
-		OS=${arg}
-		;;
-	esac
+    "clean")
+        ${MAKE} clean
+        ;;
+    "noseccomp")
+        ;;
+    "seccomp")
+        appendvar FILTER "-DSECCOMP"
+        appendvar TOOLS_LDLIBS "-lseccomp"
+        ;;
+    "noexecveat")
+        ;;
+    "execveat")
+        appendvar FILTER "-DEXECVEAT"
+        ;;
+    "deterministic"|"det")
+        DETERMINISTIC="deterministic"
+        ;;
+    "test"|"tests")
+        RUNTESTS="test"
+        ;;
+    "notest"|"notests")
+        RUNTESTS="notest"
+        ;;
+    "tools")
+        MAKETOOLS="yes"
+        ;;
+    "notools")
+        MAKETOOLS="no"
+        ;;
+    *)
+        OS=${arg}
+        ;;
+    esac
 done
 
 set -e
 
 if [ "${OS}" = "unknown" ]; then
-	die "Unknown or unsupported platform"
+    die "Unknown or unsupported platform"
 fi
 
 [ -f platform/${OS}/platform.sh ] && . platform/${OS}/platform.sh
@@ -279,44 +273,48 @@ CPPFLAGS="${EXTRA_CPPFLAGS} ${FILTER}" \
 # build lkl
 echo "=== Linux build LINUX_SRCDIR=${LKLSRC} ==="
 (
+    [ -d ${LKLSRC} ] || git clone https://github.com/lkl/linux.git ${LKLSRC}
+
     # FIXME: kind of a hack, do something with modules instead?
     cp kernel/stdio.c ${LKLSRC}/arch/lkl/kernel/stdio.c
     sed -i '$ a obj-y += stdio.o' ${LKLSRC}/arch/lkl/kernel/Makefile
 
-	cd ${LKLSRC}
-	set -e
-	set -x
-	cd tools/lkl
-	make clean
+    cd ${LKLSRC}
+    set -e
+    set -x
+    cd tools/lkl
     make -C ../.. ARCH=lkl defconfig
     sed -ie 's/^CONFIG_BTRFS_FS=y/# CONFIG_BTRFS_FS is not set/' ../../.config
     make -C ../.. ARCH=lkl install INSTALL_PATH=$PWD
-	make CFLAGS='-Iinclude -Wall -g -O2 -Wextra -Wno-unused-parameter -Wno-missing-field-initializers -fno-strict-aliasing -Wno-missing-field-initializers -fno-strict-aliasing -U_FORTIFY_SOURCE' lib/liblkl.a
-	cd ../../
-	make headers_install ARCH=lkl O=${STAGEDIR}/lkl-linux
+    make CFLAGS='-Iinclude -Wall -g -O2 -Wextra -Wno-unused-parameter -Wno-missing-field-initializers -fno-strict-aliasing -Wno-missing-field-initializers -fno-strict-aliasing -U_FORTIFY_SOURCE' lib/liblkl.a
+    cd ../../
+    make headers_install ARCH=lkl O=${STAGEDIR}/lkl-linux
     cp arch/lkl/include/asm/syscalls.h ${STAGEDIR}/lkl-linux/usr/include/asm/
 
     # undo hack
     rm ${LKLSRC}/arch/lkl/kernel/stdio.c
     sed -i '/obj-y += stdio.o/d' ${LKLSRC}/arch/lkl/kernel/Makefile
 
-	set +e
-	set +x
+    set +e
+    set +x
 )
 
 # build musl libc for Linux
 (
-	set -x
-	echo "=== building musl ==="
-	cd ../musl
-	LKL_HEADER="${STAGEDIR}/lkl-linux/"
-	CIRCLE_TEST_REPORTS="${CIRCLE_TEST_REPORTS-./}"
-	./configure --with-lkl=${LKL_HEADER} --disable-shared --enable-debug \
-		    --disable-optimize --prefix=${BUILDDIR}/musl 2>&1 | tee $CIRCLE_TEST_REPORTS/log-conf.txt
-	make install 2>&1 | tee $CIRCLE_TEST_REPORTS/log-make.txt
-	# install libraries
-	${INSTALL-install} -d ${OUTDIR}/lib
-	${INSTALL-install} ${BUILDDIR}/musl/lib/libpthread.a ${BUILDDIR}/musl/lib/libcrypt.a ${OUTDIR}/lib
+
+    [ -d ${MUSLSRC} ] || git clone https://github.com/marklee77/musl.git ${MUSLSRC}
+
+    set -x
+    echo "=== building musl ==="
+    cd ${MUSLSRC}
+    git checkout origin/franken
+    LKL_HEADER="${STAGEDIR}/lkl-linux/"
+    [ -f config.mak ] || ./configure --with-lkl=${LKL_HEADER} --disable-shared \
+            --enable-debug --disable-optimize --prefix=${BUILDDIR}/musl
+    make install
+    # install libraries
+    ${INSTALL-install} -d ${OUTDIR}/lib
+    ${INSTALL-install} ${BUILDDIR}/musl/lib/libpthread.a ${BUILDDIR}/musl/lib/libcrypt.a ${OUTDIR}/lib
 )
 
 # install headers
@@ -331,36 +329,36 @@ appendvar EXTRA_CPPFLAGS "-DCONFIG_LKL"
 appendvar EXTRA_CFLAGS "-DCONFIG_LKL"
 
 CFLAGS="${EXTRA_CFLAGS} ${DBG_F} ${HUGEPAGESIZE} ${FRANKEN_CFLAGS}" \
-	AFLAGS="${EXTRA_AFLAGS} ${DBG_F}" \
-	ASFLAGS="${EXTRA_AFLAGS} ${DBG_F}" \
-	LDFLAGS="${EXTRA_LDFLAGS}" \
-	CPPFLAGS="${EXTRA_CPPFLAGS}" \
-	BUILDDIR="${BUILDDIR}" \
-	STAGEDIR="${STAGEDIR}" \
-	${MAKE} ${STDJ} ${OS} -C platform
+    AFLAGS="${EXTRA_AFLAGS} ${DBG_F}" \
+    ASFLAGS="${EXTRA_AFLAGS} ${DBG_F}" \
+    LDFLAGS="${EXTRA_LDFLAGS}" \
+    CPPFLAGS="${EXTRA_CPPFLAGS}" \
+    BUILDDIR="${BUILDDIR}" \
+    STAGEDIR="${STAGEDIR}" \
+    ${MAKE} ${STDJ} ${OS} -C platform
 
 # should clean up how deterministic build is done
 if [ ${DETERMINSTIC-x} = "deterministic" ]
 then
-	CFLAGS="${EXTRA_CFLAGS} ${DBG_F} ${HUGEPAGESIZE} ${FRANKEN_CFLAGS}" \
-	AFLAGS="${EXTRA_AFLAGS} ${DBG_F}" \
-	ASFLAGS="${EXTRA_AFLAGS} ${DBG_F}" \
-	LDFLAGS="${EXTRA_LDFLAGS}" \
-	CPPFLAGS="${EXTRA_CPPFLAGS}" \
-	BUILDDIR="${BUILDDIR}" \
-	STAGEDIR="${STAGEDIR}" \
-	${MAKE} deterministic -C platform
+    CFLAGS="${EXTRA_CFLAGS} ${DBG_F} ${HUGEPAGESIZE} ${FRANKEN_CFLAGS}" \
+    AFLAGS="${EXTRA_AFLAGS} ${DBG_F}" \
+    ASFLAGS="${EXTRA_AFLAGS} ${DBG_F}" \
+    LDFLAGS="${EXTRA_LDFLAGS}" \
+    CPPFLAGS="${EXTRA_CPPFLAGS}" \
+    BUILDDIR="${BUILDDIR}" \
+    STAGEDIR="${STAGEDIR}" \
+    ${MAKE} deterministic -C platform
 fi 
 
 CFLAGS="${EXTRA_CFLAGS} ${DBG_F} ${HUGEPAGESIZE} ${FRANKEN_CFLAGS}" \
-	AFLAGS="${EXTRA_AFLAGS} ${DBG_F}" \
-	ASFLAGS="${EXTRA_AFLAGS} ${DBG_F}" \
-	LDFLAGS="${EXTRA_LDFLAGS}" \
-	CPPFLAGS="${EXTRA_CPPFLAGS} ${FRANKEN_FLAGS}" \
+    AFLAGS="${EXTRA_AFLAGS} ${DBG_F}" \
+    ASFLAGS="${EXTRA_AFLAGS} ${DBG_F}" \
+    LDFLAGS="${EXTRA_LDFLAGS}" \
+    CPPFLAGS="${EXTRA_CPPFLAGS} ${FRANKEN_FLAGS}" \
     LKLSRC="${LKLSRC}" \
-	BUILDDIR="${BUILDDIR}" \
-	STAGEDIR="${STAGEDIR}" \
-	${MAKE} ${STDJ} -C franken
+    BUILDDIR="${BUILDDIR}" \
+    STAGEDIR="${STAGEDIR}" \
+    ${MAKE} ${STDJ} -C franken
 
 ALL_LIBS=${LKLSRC}/tools/lkl/lib/liblkl.a
 
@@ -372,38 +370,38 @@ mkdir -p ${BUILDDIR}/explode/kernel
 mkdir -p ${BUILDDIR}/explode/franken
 mkdir -p ${BUILDDIR}/explode/platform
 (
-	# explode kernel specific libc
-	cd ${BUILDDIR}/explode/musl
-	${AR-ar} x ${BUILDDIR}/musl/lib/libc.a
+    # explode kernel specific libc
+    cd ${BUILDDIR}/explode/musl
+    ${AR-ar} x ${BUILDDIR}/musl/lib/libc.a
 
-	# some franken .o file names conflict with libc
-	cd ${BUILDDIR}/explode/franken
-	${AR-ar} x ${STAGEDIR}/lib/libfranken.a
-	for f in *.o
-	do
-		[ -f ../musl/$f ] && mv $f franken_$f
-	done
+    # some franken .o file names conflict with libc
+    cd ${BUILDDIR}/explode/franken
+    ${AR-ar} x ${STAGEDIR}/lib/libfranken.a
+    for f in *.o
+    do
+        [ -f ../musl/$f ] && mv $f franken_$f
+    done
 
-	# some platform .o file names conflict with libc
-	cd ${BUILDDIR}/explode/platform
-	${AR-ar} x ${STAGEDIR}/lib/libplatform.a
-	for f in *.o
-	do
-		[ -f ../libc/$f ] && mv $f platform_$f
-	done
+    # some platform .o file names conflict with libc
+    cd ${BUILDDIR}/explode/platform
+    ${AR-ar} x ${STAGEDIR}/lib/libplatform.a
+    for f in *.o
+    do
+        [ -f ../libc/$f ] && mv $f platform_$f
+    done
 
-	cd ${BUILDDIR}/explode/kernel
-	for f in ${ALL_LIBS}
-	do
-		${AR-ar} x $f
-	done
+    cd ${BUILDDIR}/explode/kernel
+    for f in ${ALL_LIBS}
+    do
+        ${AR-ar} x $f
+    done
     # FIXME: overriding these files for now...
     rm -f ${BUILDDIR}/explode/kernel/posix-host.o
     rm -f ${BUILDDIR}/explode/kernel/virtio_net.o
-	${CC-cc} ${EXTRA_LDFLAGS} -nostdlib -Wl,-r *.o -o kernel.o
+    ${CC-cc} ${EXTRA_LDFLAGS} -nostdlib -Wl,-r *.o -o kernel.o
 
-	cd ${BUILDDIR}/explode
-	${AR-ar} cr libc.a kernel/kernel.o musl/*.o franken/*.o platform/*.o
+    cd ${BUILDDIR}/explode
+    ${AR-ar} cr libc.a kernel/kernel.o musl/*.o franken/*.o platform/*.o
 )
 
 # install to OUTDIR
@@ -443,28 +441,23 @@ COMPILER="${TOOL_PREFIX}-gcc"
 ( cd ${BINDIR}
   ln -s ${COMPILER} ${TOOL_PREFIX}-cc
 )
-printf "#!/bin/sh\n\nexec ${AR-ar} \"\$@\"\n" > ${BINDIR}/${TOOL_PREFIX}-ar
-printf "#!/bin/sh\n\nexec ${NM-nm} \"\$@\"\n" > ${BINDIR}/${TOOL_PREFIX}-nm
-printf "#!/bin/sh\n\nexec ${OBJCOPY-objcopy} \"\$@\"\n" > ${BINDIR}/${TOOL_PREFIX}-objcopy
 chmod +x ${BINDIR}/${TOOL_PREFIX}-*
 
 # test for duplicated symbols
-
 DUPSYMS=$(nm ${OUTDIR}/lib/libc.a | grep ' T ' | sed 's/.* T //g' | sort | uniq -d )
-
 if [ -n "${DUPSYMS}" ]
 then
-	printf "WARNING: Duplicate symbols found:\n"
-	echo ${DUPSYMS}
-	#exit 1
+    printf "WARNING: Duplicate symbols found:\n"
+    echo ${DUPSYMS}
+    #exit 1
 fi
 
 # Always make tests to exercise compiler
 CC="${BINDIR}/${COMPILER}" \
-	OUTDIR="${OUTDIR}" \
-	BUILDDIR="${BUILDDIR}" \
-	BINDIR="${BINDIR}" \
-	${MAKE} ${STDJ} -C tests
+    OUTDIR="${OUTDIR}" \
+    BUILDDIR="${BUILDDIR}" \
+    BINDIR="${BINDIR}" \
+    ${MAKE} ${STDJ} -C tests
 
 # test for executable stack
 readelf -lW ${BUILDDIR}/tests/hello | grep RWE 1>&2 && echo "WARNING: writeable executable section (stack?) found" 1>&2
@@ -476,10 +469,10 @@ ${MAKE} -C tests/iputils clean
 
 if [ ${RUNTESTS} = "test" ]
 then
-	CC="${BINDIR}/${COMPILER}" \
-		OUTDIR="${OUTDIR}" \
-		BUILDDIR="${BUILDDIR}" \
-		BINDIR="${BINDIR}" \
-		${MAKE} -C tests run
+    CC="${BINDIR}/${COMPILER}" \
+        OUTDIR="${OUTDIR}" \
+        BUILDDIR="${BUILDDIR}" \
+        BINDIR="${BINDIR}" \
+        ${MAKE} -C tests run
 
 fi
